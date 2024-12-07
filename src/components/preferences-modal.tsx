255062530322c178bc244a93a96d4f73d76088d6
linzheng <alidlg9045@126.com>
2024-12-07 14:31:14 +0800

    feat: workspace's preferences

---
+++ src/components/preferences-modal.tsx
@@ -0,0 +1,129 @@
+import { DialogClose } from "@radix-ui/react-dialog"
+import { Trash } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { toast } from "sonner"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { useRemoveWorkspace } from "@/features/workspaces/api/use-remove-workspace"
+import { useUpdateWorkspace } from "@/features/workspaces/api/use-update-workspace"
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id"
+import { useConfirm } from "@/hooks/use-confirm"
+
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+
+interface PreferencesModalProps {
+  isOpen: boolean
+  onClose: () => void
+  initialValue: string
+}
+
+const PreferencesModal = ({ isOpen, onClose, initialValue }: PreferencesModalProps) => {
+  const workspaceId = useWorkspaceId()
+  const router = useRouter()
+  const [name, setName] = useState(initialValue)
+  const [isEditing, setIsEditing] = useState(false)
+  const [DeleteDialog, confirm] = useConfirm(
+    'Delete Workspace',
+    'Are you sure you want to delete this workspace?',
+    'destructive'
+  )
+
+  const { mutate: updateWorkspace, isPending: isUpdating } = useUpdateWorkspace()
+  const { mutate: removeWorkspace, isPending: isRemoving } = useRemoveWorkspace()
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    await updateWorkspace({ id: workspaceId, name }, {
+      onSuccess: () => {
+        toast.success('Workspace updated successfully')
+        setIsEditing(false)
+      },
+      onError: () => {
+        toast.error('Failed to update workspace')
+      },
+    })
+  }
+
+  async function handleRemoveWorkspace() {
+    const ok = await confirm()
+    if (!ok) return
+
+    await removeWorkspace({ id: workspaceId }, {
+      onSuccess: () => {
+        toast.success('Workspace deleted successfully')
+        onClose()
+        router.replace('/')
+      },
+      onError: () => {
+        toast.error('Failed to delete workspace')
+      },
+    })
+  }
+
+  return (
+    <>
+      <DeleteDialog />
+      <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent className="bg-white">
+          <DialogHeader>
+            <DialogTitle className="text-xl font-medium">{initialValue}</DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col gap-4">
+            <div className="flex justify-between items-center bg-white hover:bg-gray-50 cursor-pointer rounded-md p-4 border shadow-sm">
+              <div className="flex flex-col justify-between gap-2">
+                <p className="text-lg font-medium">Workspace Name</p>
+                <span className="text-sm">{initialValue}</span>
+              </div>
+              <Dialog open={isEditing} onOpenChange={setIsEditing}>
+                <DialogTrigger asChild>
+                  <p className="text-sm text-sky-500 hover:underline">
+                    Edit
+                  </p>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="text-xl font-medium">Edit Workspace Name</DialogTitle>
+                  </DialogHeader>
+                  <form className="space-y-4" onSubmit={handleSubmit}>
+                    <Input
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      disabled={isUpdating}
+                      minLength={3}
+                      autoFocus
+                      required
+                      placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+                    />
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant="outline">Cancel</Button>
+                      </DialogClose>
+                      <Button type="submit" disabled={isUpdating}>Save</Button>
+                    </DialogFooter>
+                  </form>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <button className="flex gap-2 bg-white hover:bg-gray-50 cursor-pointer rounded-md p-4 text-rose-500 border shadow-sm" onClick={handleRemoveWorkspace} disabled={isRemoving}>
+              <Trash className="size-4" />
+              <p className="text-sm">
+                Delete Workspace
+              </p>
+            </button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}
+
+export default PreferencesModal;
--- src/components/workspace-header.tsx
+++ src/components/workspace-header.tsx
@@ -1,5 +1,7 @@
 import { ChevronDown, ListFilter, SquarePen } from "lucide-react";
+import { useState } from "react";
 
+import PreferencesModal from "@/components/preferences-modal"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -18,64 +20,68 @@ interface WorkspaceHeaderProps {
 }
 
 const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
+  const [preferencesOpen, setPreferencesOpen] = useState(false)
 
   return (
-    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="transparent" size="sm" className="flex items-center text-lg w-auto p-1.5 overflow-hidden">
-            <span className="truncate">
-              {workspace?.name}
-            </span>
-            <ChevronDown className="size-4 shrink-0 ml01" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent side="bottom" align="start" className="w-64">
-          <DropdownMenuItem>
-            <div className="size-9 rounded-md text-lg bg-[#616061] text-white relative overflow-hidden flex items-center justify-center cursor-pointer">
-              {workspace.name.charAt(0).toUpperCase()}
-            </div>
-            <div className="flex flex-col items-start">
-              <p className="truncate font-bold">
-                {workspace.name}
-              </p>
-              <p className="text-xs text-muted-foreground">
-                Active Workspace
-              </p>
-            </div>
-          </DropdownMenuItem>
-          {
-            isAdmin && (
-              <>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <p className="truncate cursor-pointer py-2">
-                    Invite people to {workspace.name}
-                  </p>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <p className="truncate cursor-pointer py-2">
-                    Preferences
-                  </p>
-                </DropdownMenuItem>
-              </>
-            )
-          }
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <div className="flex items-center gap-0.5">
-        <Hint label="Filter conversations" side="bottom">
-          <Button variant="transparent" size="iconSm">
-            <ListFilter className="size-4" />
-          </Button>
-        </Hint>
-        <Hint label="New Message" side="bottom">
-          <Button variant="transparent" size="iconSm">
-            <SquarePen className="size-4" />
-          </Button>
-        </Hint>
+    <>
+      <PreferencesModal isOpen={preferencesOpen} onClose={() => setPreferencesOpen(false)} initialValue={workspace.name} />
+      <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="transparent" size="sm" className="flex items-center text-lg w-auto p-1.5 overflow-hidden">
+              <span className="truncate">
+                {workspace?.name}
+              </span>
+              <ChevronDown className="size-4 shrink-0 ml01" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent side="bottom" align="start" className="w-64">
+            <DropdownMenuItem>
+              <div className="size-9 rounded-md text-lg bg-[#616061] text-white relative overflow-hidden flex items-center justify-center cursor-pointer">
+                {workspace.name.charAt(0).toUpperCase()}
+              </div>
+              <div className="flex flex-col items-start">
+                <p className="truncate font-bold">
+                  {workspace.name}
+                </p>
+                <p className="text-xs text-muted-foreground">
+                  Active Workspace
+                </p>
+              </div>
+            </DropdownMenuItem>
+            {
+              isAdmin && (
+                <>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <p className="truncate cursor-pointer py-2">
+                      Invite people to {workspace.name}
+                    </p>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={() => setPreferencesOpen(true)}>
+                    <p className="truncate cursor-pointer py-2">
+                      Preferences
+                    </p>
+                  </DropdownMenuItem>
+                </>
+              )
+            }
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <div className="flex items-center gap-0.5">
+          <Hint label="Filter conversations" side="bottom">
+            <Button variant="transparent" size="iconSm">
+              <ListFilter className="size-4" />
+            </Button>
+          </Hint>
+          <Hint label="New Message" side="bottom">
+            <Button variant="transparent" size="iconSm">
+              <SquarePen className="size-4" />
+            </Button>
+          </Hint>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
