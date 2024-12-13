0482b0e50a960bbdc89e1c687bc912923b0e099b
linzheng <alidlg9045@126.com>
2024-12-13 22:51:10 +0800

    feat: schema finalization

--- convex/message.ts
+++ convex/message.ts
@@ -16,6 +16,7 @@ export const create = mutation({
     image: v.optional(v.id("_storage")),
     workspaceId: v.id("workspaces"),
     channelId: v.optional(v.id("channels")),
+    conversationId: v.optional(v.id("conversations")),
     parentMessageId: v.optional(v.id("messages")),
   },
   handler: async (ctx, args) => {
@@ -31,12 +32,30 @@ export const create = mutation({
       throw new Error("Unauthorized")
     }
 
+    let _conversationId = args.conversationId
+
+    // only possible if we are replying in a thread in 1:1 conversation
+    // 1:1 回复
+    // 1. 不在频道中
+    // 2. 没有对话 id
+    // 3. 有父消息 id
+    if (!args.channelId && !args.conversationId && args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId)
+
+      if (!parentMessage) {
+        throw new Error("Parent message not found")
+      }
+
+      _conversationId = parentMessage.conversationId
+    }
+
     const messageId = await ctx.db.insert("messages", {
       memberId: member._id,
       workspaceId: args.workspaceId,
       body: args.body,
       image: args.image,
       channelId: args.channelId,
+      conversationId: _conversationId,
       parentMessageId: args.parentMessageId,
       updatedAt: Date.now(),
     })
